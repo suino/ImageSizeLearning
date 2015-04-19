4364efe42d2d4193fd312fc3860ebef0afd7511a
Suino <violet1101@gmail.com>
2015-04-19 14:33:35 +0900

    add spinner

--- app/src/main/java/info/suino/imagesizelearning/MainActivity.java
+++ app/src/main/java/info/suino/imagesizelearning/MainActivity.java
@@ -4,16 +4,127 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView = (ImageView) findViewById(R.id.image_view);
+        assignHeightSpinner();
+        assignImageSpinner();
+        assignScaleSpinner();
     }
 
+    private void assignHeightSpinner() {
+        String defaultItem = "Change LayoutParams";
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.add(defaultItem);
+        adapter.add("WRAP_CONTENT");
+        adapter.add("MATCH_PARENT");
+        Spinner spinner = (Spinner) findViewById(R.id.height_spinner);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Spinner spinner = (Spinner) parent;
+                String name = (String) spinner.getSelectedItem();
+
+                int height = mImageView.getLayoutParams().height;
+                if (name.equals("WRAP_CONTENT")) {
+                    height = RelativeLayout.LayoutParams.WRAP_CONTENT;
+                } else if (name.equals("MATCH_PARENT")) {
+                    height = RelativeLayout.LayoutParams.MATCH_PARENT;
+                }
+                RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                        mImageView.getLayoutParams().width,
+                        height
+                );
+                layoutParams.addRule(RelativeLayout.BELOW, R.id.spinner_group);
+                mImageView.setLayoutParams(layoutParams);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void assignImageSpinner() {
+        final String defaultItem = "Change Image";
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.add(defaultItem);
+        adapter.add("縦長");
+        adapter.add("横長");
+        Spinner spinner = (Spinner) findViewById(R.id.image_spinner);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Spinner spinner = (Spinner) parent;
+                String name = (String) spinner.getSelectedItem();
+                if (name.equals(defaultItem)) return;
+
+                if (name.equals("縦長")) {
+                    mImageView.setImageResource(R.drawable.tatenaga_large);
+                } else if(name.equals("横長")){
+                    mImageView.setImageResource(R.drawable.yokonaga_large);
+                }
+                RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                        mImageView.getLayoutParams().width,
+                        mImageView.getLayoutParams().height
+                );
+                layoutParams.addRule(RelativeLayout.BELOW, R.id.spinner_group);
+                mImageView.setLayoutParams(layoutParams);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void assignScaleSpinner() {
+        final String defaultItem = "Change ScaleType";
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.add(defaultItem);
+        for ( ImageView.ScaleType scaleType : ImageView.ScaleType.values()) {
+            adapter.add(scaleType.name());
+        }
+        Spinner spinner = (Spinner) findViewById(R.id.scale_spinner);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Spinner spinner = (Spinner) parent;
+                String name = (String) spinner.getSelectedItem();
+                if (!name.equals(defaultItem)) {
+                    mImageView.setScaleType(ImageView.ScaleType.valueOf(name));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
